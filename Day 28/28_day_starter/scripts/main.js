c186ced39f7057ca08e346619c6d26564ca3c25c
tamsay <longetemitope@gmail.com>
2020-02-16 16:15:08 +0100

    Day 28 project finally completed with all functionalities working. It took some time getting it to work but the good thing is that I was able to figure it out

--- Day 28/28_day_starter/scripts/main.js
+++ Day 28/28_day_starter/scripts/main.js
@@ -1,22 +1,18 @@
 let validationDiv = document.querySelector('#validationDiv');
-
+let input = document.querySelectorAll('.input')
 let resultSection = document.querySelector('#leaderBoard')
 
 let sortScore =()=>{
     let scores = document.querySelectorAll('.score')
     let sortedScores = [...scores].sort((a,b)=>{
         return b.textContent - a.textContent
-    })
-    console.log(sortedScores);
-    // resultSection.appendChild(sortedScores)
-   // return sortedScores
-   [...sortedScores].map(element=>{
+    })   
+   sortedScores.map(element=>{
        resultSection.appendChild(element.parentNode);
    })
 }
 
 let deleteProfile=(num)=>{
-    console.log(num)    
             let playerDetails = document.querySelector(`#playerDetails${num}`)
             resultSection.removeChild(playerDetails)
 }
@@ -39,49 +35,63 @@ let createProfile=(num)=>{
 
     if(firstName.value === '' || lastName.value === '' || country.value === '' || playerScore.value === ''){
         console.log('All fields are required')
+        validationDiv.style.display = 'block'
         validationDiv.innerText = 'All fields are required'
+
+        for(elements of input){
+            if (elements.value === ''){
+                elements.style.border = '1px solid red'
+            }
+            else{
+                elements.style.border = '1px solid green'
+            }
+        }
     }
 
     else{
-    validationDiv.innerText = ''
+    validationDiv.style.display = 'none'
+    for(elements of input){
+            elements.style.border = '1px solid green'
+    }
+
     let playerDetails = document.createElement('div')
     playerDetails.id = `playerDetails${num}`
     playerDetails.className = 'playerDetails'
 
     let fullnameDiv = document.createElement('div')
     fullnameDiv.id = `fullname${num}`
-
-    let fNameDiv = document.querySelector(`#fullname${num}`)
-    fullnameDiv.innerText = firstName.value;
-
+    fullnameDiv.className = 'fullname'
+    fullnameDiv.innerText = `${firstName.value.toUpperCase()} ${ lastName.value.toUpperCase()}`;
     playerDetails.appendChild(fullnameDiv);
+
+    let dateTime = document.createElement('p')
+    dateTime.className = 'dateTime'
+    let now = new Date()
+    let date = now.toDateString();
+    let time = now.toLocaleTimeString()
+    dateTime.innerText = `${date}  ${time}`;
+    fullnameDiv.appendChild(dateTime);
 
     let finalPlayerCountry = document.createElement('div')
     finalPlayerCountry.id = `playerCountry${num}`
-
-    let playerCountry = document.querySelector(`#playerCountry${num}`)
-    finalPlayerCountry.innerText = country.value;
-
-
+    finalPlayerCountry.className = 'playerCountry'
+    finalPlayerCountry.innerText = country.value.toUpperCase();
     playerDetails.appendChild(finalPlayerCountry)
 
     let finalScore = document.createElement('div')
     finalScore.id = `finalScore${num}`
     finalScore.className = 'score'
-
-    let score = document.querySelector(`#finalScore${num}`)
     finalScore.innerText = playerScore.value;
-
     playerDetails.appendChild(finalScore)
     
-
     let buttonsDiv = document.createElement('div')
     buttonsDiv.id = `buttonsDiv${num}`
+    buttonsDiv.className = 'buttonsDiv'
     playerDetails.appendChild(buttonsDiv)
 
     let deletePlayerBtn = document.createElement('button')
     deletePlayerBtn.id = `deletePlayer${num}`
-    deletePlayerBtn.className = 'delete'
+    deletePlayerBtn.classList = 'delete modifyBtn'
     deletePlayerBtn.addEventListener('click', ()=>{
         deleteProfile(num);
     })
@@ -89,6 +99,8 @@ let createProfile=(num)=>{
 
     let addScoreBtn = document.createElement('button')
     addScoreBtn.id = `addScore${num}`
+    addScoreBtn.classList = 'modifyBtn'
+    addScoreBtn.innerText = '+5'
     buttonsDiv.appendChild(addScoreBtn)
     addScoreBtn.addEventListener('click', ()=>{
         addFive(num);
@@ -96,6 +108,8 @@ let createProfile=(num)=>{
 
     let subtractScoreBtn = document.createElement('button')
     subtractScoreBtn.id = `subtractScore${num}`
+    subtractScoreBtn.classList = 'modifyBtn'
+    subtractScoreBtn.innerText = '-5'
     buttonsDiv.appendChild(subtractScoreBtn)
     subtractScoreBtn.addEventListener('click', ()=>{
         subtractFive(num);
@@ -104,8 +118,6 @@ let createProfile=(num)=>{
     resultSection.appendChild(playerDetails)
     sortScore()
     }
-
-    
 }
 
 let addPlayerBtn = document.querySelector('#addPlayer');
