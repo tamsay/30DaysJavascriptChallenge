756be00dc944c9c094596fab35310e544c681f84
tamsay <longetemitope@gmail.com>
2020-02-19 20:51:56 +0100

    Still on the Project 2 of Day 30. I've made considerable progress and currently have about 20% work left to do

--- Day 30/30_day_starters/project_2/scripts/main.js
+++ Day 30/30_day_starters/project_2/scripts/main.js
@@ -1,7 +1,3 @@
-for (items of countries){
-    // console.log(items.name.length)
-}
-
 
 let header = document.querySelector('header')
 let totalCountriesSpan = document.querySelector('#totalCountries')
@@ -29,9 +25,12 @@ let ownerInfo = document.querySelector('#ownerInfo')
 let topArrow  = document.querySelector('#topArrow')
 
 let createAllCountries =(element)=>{
-    for(x=0; x < countries.length; x++){
+    fullResultSection.innerText = ''
+
+    for(x=0; x < element.length; x++){
         let countryDiv = document.createElement('div')
             countryDiv.id = `countryDiv${x}`;
+            countryDiv.className = 'countryDiv'
             countryDiv.style.backgroundColor = 'pink'
 
         let flagDiv = document.createElement('div')
@@ -43,6 +42,7 @@ let createAllCountries =(element)=>{
 
         let countryName = document.createElement('p')
             countryName.id = `countryName${x}`
+            countryName.className = 'countryName'
             countryName.style.border = '1px solid blue'
             countryName.innerText = element[x].name
             countryDiv.appendChild(countryName)
@@ -50,7 +50,15 @@ let createAllCountries =(element)=>{
         let countryCapital = document.createElement('p')
             countryCapital.id = `countryCapital${x}`
             countryCapital.style.border = '1px solid green'
-            countryCapital.innerText = `Capital: ${element[x].capital}`
+
+        let countryCapitalSpan = document.createElement('span')
+            countryCapitalSpan.innerText = 'Capital: '
+            countryCapital.appendChild(countryCapitalSpan)
+        
+        let countryCapitalValue = document.createElement('span')
+            countryCapitalValue.className = 'countryCapital'
+            countryCapitalValue.innerText = `${element[x].capital}`
+            countryCapital.appendChild(countryCapitalValue);
             countryDiv.appendChild(countryCapital)
 
         let languages = document.createElement('p')
@@ -62,27 +70,181 @@ let createAllCountries =(element)=>{
         let population = document.createElement('p')
             population.id = `population${x}`
             population.style.border = '1px solid yellow'
-            population.innerText = `Population: ${element[x].population}`
+
+        let populationSpan = document.createElement('span')
+            populationSpan.innerText = 'Population: '
+            population.appendChild(populationSpan)
+        
+        let populationValue = document.createElement('span')
+            populationValue.className = 'population'
+            populationValue.innerText = `${element[x].population}`
+            population.appendChild(populationValue);
             countryDiv.appendChild(population)
 
-        // countryDiv.innerText = 
         fullResultSection.appendChild(countryDiv)
+        // displayGraph(10)
     }
 }
 createAllCountries(countries)
 
+
+searchBar.addEventListener('input', ()=>{
+    
+        let input = searchBar.value.toLowerCase();
+        let newArray = []
+        countries.filter(items=>{
+            if(items.name.toLowerCase().includes(input) || items.capital.toLowerCase().includes(input) || items.languages.join(',').toLowerCase().includes(input)){
+                newArray.push(items)
+            } 
+        })
+        createAllCountries(newArray)    
+        // displayGraph(newArray)
+})
+
+
+let countName = 0;
 let sortCountriesByName =()=>{
-    fullResultSection.innerText = ''
-   // let sortedCountry = [];
-    let sortedCountry = countries.reverse()//=>{
-        // console.log(a.name)
-        // console.log(b.name)
-     //   return b.name - a.name
-   // })
-    console.log(sortedCountry)
-    createAllCountries(sortedCountry);
+        let countryName = document.querySelectorAll('.countryName');
+
+        if(countName%2 === 0){
+            let sortedCountry = [...countryName].sort((b,a)=>{
+                let nameA=a.textContent.toLowerCase(), nameB=b.textContent.toLowerCase()
+                if (nameA < nameB) //sort string ascending
+                    return -1 
+                if (nameA > nameB)
+                    return 1
+                return 0 //default return value (no sorting)
+            })
+        fullResultSection.innerText = ''
+        for (x=0; x < sortedCountry.length; x++){
+            fullResultSection.appendChild(sortedCountry[x].parentElement)
+        }
+        countName++;
+        }
+        else{
+            let sortedCountry = [...countryName].sort((a,b)=>{
+                let nameA=a.textContent.toLowerCase(), nameB=b.textContent.toLowerCase()
+                if (nameA < nameB) //sort string ascending
+                    return -1 
+                if (nameA > nameB)
+                    return 1
+                return 0 //default return value (no sorting)
+            })
+        fullResultSection.innerText = ''
+        for (x=0; x < sortedCountry.length; x++){
+            fullResultSection.appendChild(sortedCountry[x].parentElement)
+        }
+        countName++
+        }
+}
+let countCapital = 0;
+let sortCountriesByCapital=()=>{
+        let countryCapital = document.querySelectorAll('.countryCapital');
+        if(countCapital % 2 === 0){
+            let sortedCapital = [...countryCapital].sort((b,a)=>{
+                let nameA=a.textContent.toLowerCase(), nameB=b.textContent.toLowerCase()
+                if (nameA < nameB) //sort string ascending
+                    return -1 
+                if (nameA > nameB)
+                    return 1
+                return 0 //default return value (no sorting)
+            })
+            fullResultSection.innerText = ''
+            for (x=0; x < sortedCapital.length; x++){
+                fullResultSection.appendChild(sortedCapital[x].parentElement.parentElement)
+            }
+            countCapital++;
+        }
+        else{
+            let sortedCapital = [...countryCapital].sort((a,b)=>{
+                let nameA=a.textContent.toLowerCase(), nameB=b.textContent.toLowerCase()
+                if (nameA < nameB) //sort string ascending
+                    return -1 
+                if (nameA > nameB)
+                    return 1
+                return 0 //default return value (no sorting)
+            })
+            fullResultSection.innerText = ''
+            for (x=0; x < sortedCapital.length; x++){
+                fullResultSection.appendChild(sortedCapital[x].parentElement.parentElement)
+            }
+            countCapital++;
+        }
 }
 
-nameBtn.addEventListener('click', sortCountriesByName)
+let countPopulation = 0;
+let sortCountriesByPopulation=()=>{
+    let countryPopulation = document.querySelectorAll('.population');
 
+    if(countPopulation%2 === 0){
+        let sortedPopulation = [...countryPopulation].sort((b,a)=>{
+            return a.textContent - b.textContent;
+        })
+        
+        fullResultSection.innerText = ''
+       
+        for (x=0; x < sortedPopulation.length; x++){
+            fullResultSection.appendChild(sortedPopulation[x].parentElement.parentElement)
+        }
+        countPopulation++;
+        return sortedPopulation;
+    }
+    else{
+        let sortedPopulation = [...countryPopulation].sort((a,b)=>{
+            return a.textContent - b.textContent;
+        })
+        
+        fullResultSection.innerText = ''
+       
+        for (x=0; x < sortedPopulation.length; x++){
+            fullResultSection.appendChild(sortedPopulation[x].parentElement.parentElement)
+        }
+        countPopulation++;
+        return sortedPopulation;
+    }
+}
+
+let displayGraph=(array)=>{
+    // let data = sortCountriesByPopulation();
+    // let data = document.querySelectorAll('.countryDiv')
+    let countryPopulation = document.querySelectorAll('.population');
+
+        let sortedPopulation = [...countryPopulation].sort((b,a)=>{
+            return a.textContent - b.textContent;
+        })
+        let topTen = sortedPopulation.slice(0,array)
+        let totalPopulation = countries.reduce((acc, arr)=>{
+            return arr.population + acc;
+        },0)
+        // console.log(totalPopulation)
+
+        let totalPopDiv = document.createElement('div')
+        totalPopDiv.id = 'masterPopulation'
+        totalPopDiv.style.display = 'flex'
+
+        let countrySpan = document.createElement('p')
+        countrySpan.innerText = 'World Population'
+        totalPopDiv.appendChild(countrySpan)
+
+        let graphSpan = document.createElement('p')
+        graphSpan.style.width = '100%'
+        graphSpan.style.backgroundColor = 'green'
+        totalPopDiv.appendChild(graphSpan)
+
+        let sizeSpan = document.createElement('p')
+        sizeSpan.innerText = `${totalPopulation}`
+        totalPopDiv.appendChild(sizeSpan)
+
+        graphicsResult.appendChild(totalPopDiv)
+
+        for (x=0; x < array; x++){
+            graphicsResult.appendChild(topTen[x].parentElement.parentElement)
+        }
+}
+displayGraph(10)
+
+
+nameBtn.addEventListener('click', sortCountriesByName)
+capitalBtn.addEventListener('click', sortCountriesByCapital)
+populationBtn.addEventListener('click', sortCountriesByPopulation)
 
